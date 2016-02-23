dbb5f91dfbe3c43d7c6bf8bbaf17ad3620613d7e
pacocampo <paco.ocampor@gmail.com>
2016-02-22 19:04:05 -0600

    delete for testing

--- traveleando/project/destino/models.py
+++ traveleando/project/destino/models.py
@@ -3,6 +3,11 @@ from category.models import *
 from usuario.models import *
 from utils.countryinfo import COUNTRY_CHOICES
 # Create your models here.
+class MexicoManager(models.Manager):
+    def get_queryset(self):
+        return super(MexicoManager, self).get_queryset().filter(pais='MX')
+
+
 class Destino(models.Model):
 
     class Meta:
@@ -16,8 +21,11 @@ class Destino(models.Model):
     nombre = models.CharField(max_length=100, blank=False)
     pais = models.CharField(max_length=2, choices=COUNTRY_CHOICES)
     continente = models.CharField(max_length=15, blank=False)
-    imagen = models.ImageField(upload_to='media/pais/')
+    imagen = models.ImageField(upload_to='pais/')
     is_active = models.BooleanField(default=True)
+
+    #Managers
+    mexico = MexicoManager()
 
     def __str__(self):
         return self.nombre
--- traveleando/project/destino/views.py
+++ traveleando/project/destino/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Destino
 # Create your views here.
+def index(request):
+	d =  Destino.mexico.all()
+	return render(request, 'index.html', {'destinos':d})
